2698176940c847ca10c3d88ae761e90ab3fd83c1
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-09-09 21:40:39 +0000

    make load to use compiled elisp file if available.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@133 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- init.el
+++ init.el
@@ -2,17 +2,13 @@
 
 (add-to-list 'load-path (file-name-directory (or load-file-name buffer-file-name)))
 
-;; (load "ergonomic_keybinding_dvorak")
-;; (load "ergonomic_keybinding_qwerty")
-
-(load "ergoemacs_keybindings_minor_mode/ergoemacs_minor_mode.el")
+(load "ergoemacs_keybindings_minor_mode/ergoemacs_minor_mode")
 (ergoemacs-mode 1)
 
-
-(load "init_functions.el")
-(load "init_keybinding.el")
-(load "init_load_packages.el")
-(load "init_settings.el")
-(load "init_clean_menus.el")
+(load "init_functions")
+(load "init_keybinding")
+(load "init_load_packages")
+(load "init_settings")
+(load "init_clean_menus")
 
 (server-start) ; this keeps emacs running just one instance. For example, a user double clicks a file, it'll just switch to a existing instance. Not sure this is best approach.
