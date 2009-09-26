a48ff41e57695d1b8a0b59632b43f292ff132d7c
davidcapello <davidcapello@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-09-26 19:40:26 +0000

    Fixed a bug introduced in the last commit: Now ergoemacs keybindings are back in the menus.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@257 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/init.el
+++ ergoemacs/init.el
@@ -4,16 +4,18 @@
 (add-to-list 'load-path 
 	     (concat (file-name-directory (or load-file-name buffer-file-name)) "../packages"))
 
+(load "ergoemacs-keybindings/ergoemacs-mode")
+
 (load "init_version")
 (load "init_functions")
 (load "init_keybinding")
 (load "init_load_packages")
 (load "init_settings")
-(load "init_clean_menus")
 
-;; The ergoemacs mode is the last thing to be initialized (so we can
-;; hook all packages, like iswitchb)
-(load "ergoemacs-keybindings/ergoemacs-mode")
+;; The ergoemacs mode is the last thing to be initialized (so we can hook all packages, like iswitchb)
 (ergoemacs-mode 1)
+
+;; After initializing ergoemacs mode, we can clean menus (so ergoemacs keybindings are shown)
+(load "init_clean_menus")
 
 ;; (server-start) ; this keeps emacs running just one instance. For example, a user double clicks a file, it'll just switch to a existing instance. Not sure this is best approach.
