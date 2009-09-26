58887e0d0108901c8ca7521571d458fa65f4830d
davidcapello <davidcapello@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-09-26 17:42:02 +0000

    iswitchb hooks now are correctly initialized.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@255 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/init.el
+++ ergoemacs/init.el
@@ -4,14 +4,16 @@
 (add-to-list 'load-path 
 	     (concat (file-name-directory (or load-file-name buffer-file-name)) "../packages"))
 
-(load "ergoemacs-keybindings/ergoemacs-mode")
-(ergoemacs-mode 1)
-
 (load "init_version")
 (load "init_functions")
 (load "init_keybinding")
 (load "init_load_packages")
 (load "init_settings")
 (load "init_clean_menus")
+
+;; The ergoemacs mode is the last thing to be initialized (so we can
+;; hook all packages, like iswitchb)
+(load "ergoemacs-keybindings/ergoemacs-mode")
+(ergoemacs-mode 1)
 
 ;; (server-start) ; this keeps emacs running just one instance. For example, a user double clicks a file, it'll just switch to a existing instance. Not sure this is best approach.
