26a02512c685d023f8216a23c969e58e1bd5e13b
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-10-01 18:26:07 +0000

    minor fixes.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@294 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- build-util/build_ergoemacs_package.el
+++ build-util/build_ergoemacs_package.el
@@ -1,21 +1,52 @@
--*- coding: utf-8 -*-
+; -*- coding: utf-8 -*-
 
-; 2009-10-01
-; run this file to build a ErgoEmacs package for installation on any emacs binary.
+;; 2009-10-01
+;; run this file to build a ErgoEmacs package for installation on any emacs binary.
 
-; steps:
-; copy the whole dir into some dest dir.
-; remove all .svn dirs.
-; remove other files and dir such as Makefile and win32-setup etc.
+;; Steps:
+;; copy the whole dir into some dest dir.
+;; remove all .svn dirs.
+;; remove other files and dir such as Makefile and win32-setup etc.
 
 (defvar sourceDir nil "The ergoemacs source code dir in repository.")
 (setq sourceDir "../")
 
 (defvar destDir nil "The output dir.")
-(setq destDir "../ergoemacs_1.6.2/")
+(setq destDir "../../ergoemacs_1.6.2/")
+
+(defun copy-directory-recursive (source-dir dest-dir)
+  "Copy a whole directory SOURCE-DIR to DEST-DIR.
+Note, the semantics of source-dir dest-dir is different from the
+unix “cp” utility.  In unix's “cp -R”, if dest-dir exists, it'll
+copy source-dir itself, else, just source-dir's children.
 
+In copy-directory-recursive, it always copy source-dir's children.
+
+In both, the dest-dir may or may not exist. If not, it'll be
+created. However, dest-dir's parent must exist.
+
+This function is based on dired-copy-file-recursive.  Behavior
+about linked files, time stamp, etc, are from that function."
+  (require 'dired-aux)
+  (dired-copy-file-recursive source-dir dest-dir nil nil nil 'always)
+  )
+
 ;; main
 (make-directory destDir t)
-(shell-command (concat "cp -R " sourceDir " " destDir) )
+(copy-directory-recursive sourceDir destDir)
+; (shell-command (concat "cp -R " sourceDir " " destDir) )
 
-(shell-command (concat "find " destDir " -name \".svn\" -type d -exec rmdir {} ';'" ) )
+(shell-command (concat "find " destDir " -depth -name \".svn\" -type d -exec rm -R {} ';'" ) )
+
+(shell-command (concat " rm -R " destDir "win32-setup"))
+(delete-file (concat destDir "Makefile"))
+(delete-file (concat destDir "build-util/build_ergoemacs_package.el"))
+
+;; byte compile elc files
+(load-file (concat destDir "build-util/byte-compile_lisp_files.el"))
+
+;; TODO
+;; ideally, change all shell calls to elisp functions so it's not dependent on shell.
+;; using elisp for build is just experimental. We can revert to unix shell in the future.
+
+;; currently, the version number is hard coded. We probably want to make use svn's tag feature for version stapm, for building both Windows release and elisp package release.
