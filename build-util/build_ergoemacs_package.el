ae31779d2f018661ab62b5229ebe4f1bfb7b7ee6
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-10-02 01:40:04 +0000

    Corrected a problem with copying a dir, by not using elisp function.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@300 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- build-util/build_ergoemacs_package.el
+++ build-util/build_ergoemacs_package.el
@@ -26,15 +26,19 @@ In both, the dest-dir may or may not exist. If not, it'll be
 created. However, dest-dir's parent must exist.
 
 This function is based on dired-copy-file-recursive.  Behavior
-about linked files, time stamp, etc, are from that function."
+about linked files, time stamp, etc, are from that function.
+
+WARNING: when copying to a existing dir with existing files, old files does not seem to get over-written.
+This is a major bug... needs research.
+Do not use this function."
   (require 'dired-aux)
   (dired-copy-file-recursive source-dir dest-dir nil nil nil 'always)
   )
 
 ;; main
 (make-directory destDir t)
-(copy-directory-recursive sourceDir destDir)
-; (shell-command (concat "cp -R " sourceDir " " destDir) )
+; (copy-directory-recursive sourceDir destDir)
+ (shell-command (concat "cp -R " sourceDir " " destDir) )
 
 (shell-command (concat "find " destDir " -depth -name \".svn\" -type d -exec rm -R {} ';'" ) )
 
