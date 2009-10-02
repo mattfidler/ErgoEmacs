be741f9916f775b6bf1aa79982decd8a1083ee88
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-10-02 03:39:28 +0000

    fixed the load path problem.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@303 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- site-lisp/site-start.el
+++ site-lisp/site-start.el
@@ -1,19 +1,31 @@
 ; -*- coding: utf-8 -*-
 
-(add-to-list 'load-path (file-name-directory (or load-file-name buffer-file-name)))
-(load "../ergoemacs/init")
+(defun fullPath-relative-to-calling-function (file-path)
+  "Returns the full path of FILE-PATH, relative to file location of this function call.
+ PATH is a file path relative to the dir of the file where this function is called.
 
-;; Note: if you have a file such as ~.emacs that tells emacs to load another file such as this file, and in the second file, you call “load” with a relative path, such as (load "../some/init"), emacs complains about not able to find the file. The error message is “File error: Cannot open load file, ../some/init”. It appears, when emacs load files, it does not change its current dir.
-;; this is extremely annoying.
-;; One way to solve it is to add the file dir into the load-path, eg. (add-to-list 'load-path (file-name-directory (or load-file-name buffer-file-name)))
-;; but that is not ideal because you don't really want the dir to be perm part of the load-path.
-;; So, i tried the following:
-;; (load-file
-;;  (expand-file-name (concat "../"
-;;                            (file-relative-name
-;;                             (file-name-directory
-;;                              (or load-file-name buffer-file-name)))
-;;                            "ergoemacs/init.elc"
-;;                            )) )
-;; after some 30 min, so far doesn't work. I abondoned on dealing with this.
-;; Xah Lee, 2009-10-01
+Example: If the file that calls fullPath-relative-to-load-path is at:
+/Users/xah/web/emacs/emacs_init.el then,
+ (fullPath-relative-to-load-path \"xyz.el\")
+returns
+ /Users/xah/web/emacs/xyz.el
+
+This function solves 2 problems.
+ (1) if you have file A, that calls the “load” on a file at B,
+ and B calls “load” on file C using a relative path, then Emacs
+ will complain about unable to find C. Because, emacs does not
+ switch current directory with “load”. To solve this, you
+ call (fullPath-relative-to-load-path ‹your path of C relative to
+ B›) in B.
+
+ (2) To know the current file's full path, emacs has 2 ways:
+ load-file-name and buffer-file-name. If the file is called by
+ “load”, then load-file-name contains the file's path, but not
+ buffer-file-name. But if the file is called by eval-buffer, then
+ load-file-name is nil. So, to know the running file's path
+ regardless how it is called, use “(or load-file-name
+ buffer-file-name)”. This is part of this function."
+  (concat (file-name-directory (or load-file-name buffer-file-name)) file-path)
+)
+
+(load (fullPath-relative-to-calling-function "../ergoemacs/init") )
