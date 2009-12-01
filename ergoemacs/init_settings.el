74ea0aa21af582897acb1243f8be080977c34d56
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-12-01 00:18:59 +0000

    moved desktop settings code in after-init-hook, from lambda into a stand alone function. Easier to manage, and user can manually call it if user starts with -Q.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@329 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/init_settings.el
+++ ergoemacs/init_settings.el
@@ -39,65 +39,83 @@
 (add-hook 'org-mode-hook 'soft-wrap-lines)
 
 ;; ----------------------------------------------------------------------------
-;; Desktop:
+;; Make emacs open all files in last emacs session.
 ;;
-;; In ErgoEmacs if the user does not open a desktop file, a default desktop
-;; file is created in ~/.emacs.d/.emacs.desktop to keep track of opened files
-;; through different sessions. Anyway it does not matter if this desktop file
-;; is overwritten by another instance.  
-;; ----------------------------------------------------------------------------
+;; This functionality is provided by desktop-save-mode (“feature”
+;; name: “desktop”). The mode is not on by default in emacs 23, and
+;; has a lot options. The following is init settings for the mode for
+;; ErgoEmacs.
+;;
+;; Goal: have emacs always auto open the set of opend files in last
+;; session, even if emacs crashed in last session or the OS crashed in
+;; last session. Also, don't bother users by asking questions like “do
+;; you want to save desktop?” or “do you want to override last session
+;; file?”, because these are annoying and terms like “session” or
+;; “desktop” are confusing to most users because it can have many
+;; meanings.
+
+;; Some tech detail: set the desktop session file at
+;; “~/.emacs.d/.emacs.desktop”.  This file is our desktop file. It
+;; will be auto created and or over-written.  if a emacs expert has
+;; other desktop session files elsewhere, he can still use or manage those.
 
 (require 'desktop)
 
+(defun desktop-settings-setup ()
+  "Some settings setup for desktop-save-mode."
+  (interactive)
+  
+  ;; At this point the desktop.el hook in after-init-hook was
+  ;; executed, so (desktop-read) is avoided.
+  (when (not (eq (emacs-pid) (desktop-owner))) ; Check that emacs did not load a desktop yet
+    ;; Here we activate the desktop mode
+    (desktop-save-mode 1)
+
+    ;; The default desktop is saved always
+    (setq desktop-save t)
+
+    ;; The default desktop is loaded anyway if it is locked
+    (setq desktop-load-locked-desktop t)
+
+    ;; Set the location to save/load default desktop
+    (setq desktop-dirname "~/.emacs.d/")
+
+    ;; Make sure that even if emacs or OS crashed, emacs
+    ;; still have last opened files.
+    (add-hook 'find-file-hook
+              (lambda ()
+                (run-with-timer 5 nil
+                                (lambda ()
+                                  ;; Reset desktop modification time so the user is not bothered
+                                  (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))
+                                  (desktop-save "~/.emacs.d/")))))
+
+    ;; Read default desktop
+    (if (file-exists-p (concat desktop-dirname desktop-base-file-name))
+        (desktop-read desktop-dirname))
+
+    ;; Add a hook when emacs is closed to we reset the desktop
+    ;; modification time (in this way the user does not get a warning
+    ;; message about desktop modifications)
+    (add-hook 'kill-emacs-hook
+              (lambda ()
+                ;; Reset desktop modification time so the user is not bothered
+                (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))))
+    )
+  )
+
 (add-hook 'after-init-hook
- (lambda ()
-   ;; No splash screen
-   (setq inhibit-startup-screen t)
+          'desktop-settings-setup
+          (lambda ()
+            ;; No splash screen
+            (setq inhibit-startup-screen t)
 
-   ;; At this point the desktop.el hook in after-init-hook was
-   ;; executed, so (desktop-read) is avoided.
-   (when (not (eq (emacs-pid) (desktop-owner))) ; Check that emacs did not load a desktop yet
-     ;; Here we activate the desktop mode
-     (desktop-save-mode 1)
-
-     ;; The default desktop is saved always
-     (setq desktop-save t)
-
-     ;; The default desktop is loaded anyway if it is locked
-     (setq desktop-load-locked-desktop t)
-
-     ;; Set the location to save/load default desktop
-     (setq desktop-dirname "~/.emacs.d/")
-
-     ;; Make sure that even if emacs or PC crashed, emacs
-     ;; still have last opened files.
-     (add-hook 'find-file-hook
-	       (lambda ()
-		 (run-with-timer 5 nil
-				 (lambda ()
-				   ;; Reset desktop modification time so the user is not bothered
-				   (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))
-				   (desktop-save "~/.emacs.d/")))))
-
-     ;; Read default desktop
-     (if (file-exists-p (concat desktop-dirname desktop-base-file-name))
-	 (desktop-read desktop-dirname))
-
-     ;; Add a hook when emacs is closed to we reset the desktop
-     ;; modification time (in this way the user does not get a warning
-     ;; message about desktop modifications)
-     (add-hook 'kill-emacs-hook
-	       (lambda ()
-		 ;; Reset desktop modification time so the user is not bothered
-		 (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))))
-     )
-
-   ;; If the *scratch* buffer is the current one, let us create a new
-   ;; empty untitled buffer to hide *scratch*
-   (if (string= (buffer-name) "*scratch*")
-       (new-empty-buffer))
-   )
- t) ;; append this hook to the tail
+            ;; If the *scratch* buffer is the current one, then create a new
+            ;; empty untitled buffer to hide *scratch*
+            (if (string= (buffer-name) "*scratch*")
+                (new-empty-buffer))
+            )
+          t) ;; append this hook to the tail
 
 ;; ----------------------------------------------------------------------------
 
