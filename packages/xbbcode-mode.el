65b9ba9c6818c1c0603a862144e49d1e281c255c
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2009-12-14 14:48:11 +0000

    minor version update.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@336 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- packages/xbbcode-mode.el
+++ packages/xbbcode-mode.el
@@ -34,11 +34,12 @@
 
 ;;; HISTORY
 
+;; version 1.1, 2009-12-14. The xbbcode-about now has clickable links, and other minor improvement.
 ;; version 1.0, 2009-12-09. First version.
 
 ;;; Code:
 
-(setq xbbcode-mode-version "1.0")
+(setq xbbcode-mode-version "1.1")
 
 (defgroup xbbcode-mode nil
   "Major mode for editing bbcode."
@@ -76,12 +77,15 @@
 (defun xbbcode-about ()
   "Describe the major mode."
   (interactive)
-  (display-message-or-buffer
-   (concat "xbbcode-mode is a major mode for editing BBCode.\n\n"
-           "You are currently using version: " xbbcode-mode-version "\n\n"
-           "To see inline documentation, type “Alt+x describe-mode”.\n\n"
-           "The home page is http://xahlee.org/emacs/xbbcode-mode.html \n\n")
-   )
+  (with-output-to-temp-buffer "*Majro Modes for Text Editing*"
+    (princ 
+     (concat "xbbcode-mode is a major mode for editing BBCode.\n\n"
+             "You are currently using version: " xbbcode-mode-version "\n\n"
+             "Author: Xah Lee\n\n"
+             "To see inline documentation, call the command `describe-mode' while in the mode.\n\n"
+             "The home page is URL `http://xahlee.org/emacs/xbbcode-mode.html' \n\n")
+     )
+    )
   )
 
 (defun xbbcode-insert-tag ()
