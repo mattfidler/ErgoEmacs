4a6de9c28dd8cc2d1520f58aa75b3be2cea8847b
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2010-06-30 16:24:38 +0000

    removed a unused and bad function copy-directory-recursive
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@442 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- build-util/build_ergoemacs_package.el
+++ build-util/build_ergoemacs_package.el
@@ -14,27 +14,7 @@
 (defvar destDir nil "The output dir.")
 (setq destDir "../../ergoemacs_1.6.2/")
 
-(defun copy-directory-recursive (source-dir dest-dir)
-  "Copy a whole directory SOURCE-DIR to DEST-DIR.
-Note, the semantics of source-dir dest-dir is different from the
-unix “cp” utility.  In unix's “cp -R”, if dest-dir exists, it'll
-copy source-dir itself, else, just source-dir's children.
 
-In copy-directory-recursive, it always copy source-dir's children.
-
-In both, the dest-dir may or may not exist. If not, it'll be
-created. However, dest-dir's parent must exist.
-
-This function is based on dired-copy-file-recursive.  Behavior
-about linked files, time stamp, etc, are from that function.
-
-WARNING: when copying to a existing dir with existing files, old files does not seem to get over-written.
-This is a major bug... needs research.
-Do not use this function."
-  (require 'dired-aux)
-  (dired-copy-file-recursive source-dir dest-dir nil nil nil 'always)
-  )
-
 ;; main
 
 
@@ -48,7 +28,6 @@ Do not use this function."
   )
 
 (make-directory destDir t)
-; (copy-directory-recursive sourceDir destDir)
  (shell-command (concat "cp -R " sourceDir " " destDir) )
 
 (shell-command (concat "find " destDir " -depth -name \".svn\" -type d -exec rm -R {} ';'" ) )
