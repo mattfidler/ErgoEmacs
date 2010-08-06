2559fef2f725f6af09da6c78caa2646475b2b0ba
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2010-08-06 10:23:03 +0000

    modified select-text-in-quote to support more matching pairs.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@449 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/ergoemacs-keybindings/functions.el
+++ ergoemacs/ergoemacs-keybindings/functions.el
@@ -64,7 +64,7 @@ If narrow-to-region is in effect, then cut that region only."
 (defun select-text-in-quote ()
   "Select text between the nearest left and right delimiters.
 Delimiters are paired characters:
- ()[]{}«»‹›“”〖〗【】「」『』〈〉《》〔〕
+ ()[]{}«»‹›“”〖〗【】「」『』（）〈〉《》〔〕⦗⦘〘〙
 
 For practical purposes, it also includes double straight quote
 \", but except single quote matching pairs ‘’, because that is
@@ -73,9 +73,9 @@ angle brackets <> as either beginning or ending pair, so that it
 is easy to get content inside html tags."
  (interactive)
  (let (b1 b2)
-   (skip-chars-backward "^<>([{“「『‹«〈《〔【〖\"")
+   (skip-chars-backward "^<>([{“「『‹«（〈《〔【〖⦗〘\"")
    (setq b1 (point))
-   (skip-chars-forward "^<>)]}”」』›»〉》〕】〗\"")
+   (skip-chars-forward "^<>)]}”」』›»）〉》〕】〗⦘〙\"")
    (setq b2 (point))
    (set-mark b1)
    )
