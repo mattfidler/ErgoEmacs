700cdfd181916d38aa1d908a2f2387e0fa0bec5d
xahlee <xahlee@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2010-12-21 02:14:43 +0000

    added 2 browser style hotkeys to Info-mode.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@599 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/init_keybinding.el
+++ ergoemacs/init_keybinding.el
@@ -6,6 +6,19 @@
 
 (global-set-key (kbd "C-S-z") 'redo)
 
+(defun browser-nav-keys ()
+  "Add some browser styled nav keys for Info-mode.
+  The following keys and mouse buttons are added:
+ 【Backspace】 and <mouse-4> for `Info-history-back'
+ 【Shift+Backspace】 and <mouse-5> for `Info-history-forward'."
+ (local-set-key (kbd "<backspace>") 'Info-history-back)
+ (local-set-key (kbd "<S-backspace>") 'Info-history-forward)
+ (local-set-key (kbd "<mouse-4>") 'Info-history-back)
+ (local-set-key (kbd "<mouse-5>") 'Info-history-forward)
+  )
+
+(add-hook 'Info-mode-hook 'browser-nav-keys)
+
 ;; In Windows, Alt+F4 closes the frame (or kill emacs if it is the last frame)
 (if (and (boundp 'w32-initialized) w32-initialized)
     (global-set-key (kbd "M-<f4>") 'close-frame))
