eee905b74fc7055155d851aeecec9a7e2deaeb7f
xahlee@gmail.com <xahlee@gmail.com@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2011-10-25 15:10:32 +0000

    functions.el modified select-text-in-quote to support more brackets.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@732 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/ergoemacs-keybindings/functions.el
+++ ergoemacs/ergoemacs-keybindings/functions.el
@@ -63,7 +63,7 @@ If narrow-to-region is in effect, then cut that region only."
 (defun select-text-in-quote ()
   "Select text between the nearest left and right delimiters.
 Delimiters are paired characters:
- () [] {} «» ‹› “” 〖〗 【】 「」 『』 （） 〈〉 《》 〔〕 ⦗⦘ 〘〙
+ () [] {} «» ‹› “” 〖〗 【】 「」 『』 （） 〈〉 《》 〔〕 ⦗⦘ 〘〙 ⦅⦆ 〚〛 ⦃⦄
 
 For practical purposes, it also includes double straight quote
 \", but not curly single quote matching pairs ‘’, because that is
@@ -72,9 +72,9 @@ angle brackets <> as either beginning or ending pair, so that it
 is easy to get content inside html tags."
  (interactive)
  (let (b1 b2)
-   (skip-chars-backward "^<>([{“「『‹«（〈《〔【〖⦗〘\"")
+   (skip-chars-backward "^<>([{“「『‹«（〈《〔【〖⦗〘⦅〚⦃\"")
    (setq b1 (point))
-   (skip-chars-forward "^<>)]}”」』›»）〉》〕】〗⦘〙\"")
+   (skip-chars-forward "^<>)]}”」』›»）〉》〕】〗⦘〙⦆〛⦄\"")
    (setq b2 (point))
    (set-mark b1)
    )
