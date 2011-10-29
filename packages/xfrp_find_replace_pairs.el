cdbcbe5ffd62ee6cf9b6758e076329ce1093f8ee
xahlee@gmail.com <xahlee@gmail.com@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2011-10-29 22:40:04 +0000

    xfrp_find_replace_pairs.el updated to version 1.4 , major update on the implementation of â€œreplace-pairs-regionâ€, and minor update on others. No user visible change.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@734 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- packages/xfrp_find_replace_pairs.el
+++ packages/xfrp_find_replace_pairs.el
@@ -31,6 +31,7 @@
 
 ;;; HISTORY
 
+;; version 1.4, 2011-10-29 • major update on the implementation of “replace-pairs-region”, and minor update on others. No user visible change.
 ;; version 1.3, 2011-09-28 • slight change to replace-pairs-in-string to improve speed. The function's user level behavior is the same.
 ;; version 1.2, 2011-08-31 • change made to replace-pairs-region so that inserting occurs only if there are changes made. The function's user level behavior is the same, except the function might be slower when the region's text is large.
 ;; version 1.1, 2011-03-14. • fixed a doc error in replace-pairs-region. • fixed a code error in replace-regexp-pairs-in-string (this fix has no change in behavior).
@@ -50,7 +51,7 @@ The search strings are not case sensitive.
 The replacement are literal and case sensitive.
 
 If you want search strings to be case sensitive, set
-case-fold-search to nil. Like this:
+`case-fold-search' to nil. Like this:
 
  (let ((case-fold-search nil)) 
    (replace-regexp-in-string-pairs …)
@@ -58,43 +59,41 @@ case-fold-search to nil. Like this:
 Once a subsring in the input string is replaced, that part is not changed again.
 For example, if the input string is “abcd”, and the pairs are
 a → c and c → d, then, result is “cbdd”, not “dbdd”.
-See also `replace-pairs-in-string-recursive'.
+If you simply want repeated replacements, use `replace-pairs-in-string-recursive'.
 
-This function calls `replace-regexp-in-string' to do its work.
-
-See also `replace-regexp-pairs-in-string'."
+See also `replace-regexp-pairs-in-string' and `replace-pairs-region'."
   ;; code outline. Replace first item in each pair to a unique random string, then replace this list to the desired string.
-  (let (ii (mystr str) (tempMapPoints '()))
+  (let (ξi (myStr str) (tempMapPoints '()))
     ;; generate a random string list for intermediate replacement
-    (setq ii 0)
-    (while (< ii (length pairs))
+    (setq ξi 0)
+    (while (< ξi (length pairs))
       ;; use rarely used unicode char to prevent match in input string
       ;; was using random number for the intermediate string. The problem is: ① there might be collision if there are hundreds or thousands of pairs. ② the random number are too long, even in hex notation, and slows down string replacement.
-      (setq tempMapPoints (cons (format "⚎%x" ii ) tempMapPoints ))
-      (setq ii (1+ ii))
+      (setq tempMapPoints (cons (format "⚎ด%x" ξi) tempMapPoints ))
+      (setq ξi (1+ ξi))
       )
 
     ;; replace each find string by corresponding item in random string list
-    (setq ii 0)
-    (while (< ii (length pairs))
-      (setq mystr (replace-regexp-in-string
-                   (regexp-quote (elt (elt pairs ii) 0))
-                   (elt tempMapPoints ii)
-                   mystr t t))
-      (setq ii (1+ ii))
+    (setq ξi 0)
+    (while (< ξi (length pairs))
+      (setq myStr (replace-regexp-in-string
+                   (regexp-quote (elt (elt pairs ξi) 0))
+                   (elt tempMapPoints ξi)
+                   myStr t t))
+      (setq ξi (1+ ξi))
       )
 
     ;; replace each random string by corresponding replacement string
-    (setq ii 0)
-    (while (< ii (length pairs))
-      (setq mystr (replace-regexp-in-string
-                   (elt tempMapPoints ii)
-                   (elt (elt pairs ii) 1)
-                   mystr t t))
-      (setq ii (1+ ii))
+    (setq ξi 0)
+    (while (< ξi (length pairs))
+      (setq myStr (replace-regexp-in-string
+                   (elt tempMapPoints ξi)
+                   (elt (elt pairs ξi) 1)
+                   myStr t t))
+      (setq ξi (1+ ξi))
       )
     
-    mystr))
+    myStr))
 
 (defun replace-regexp-pairs-in-string (str pairs &optional fixedcase)
   "Replace string STR recursively by regex find/replace pairs PAIRS sequence.
@@ -113,36 +112,69 @@ If you want the regex to be case sensitive, set the global variable case-fold-se
 This function calls `replace-regexp-in-string' to do its work.
 
 See also `replace-pairs-in-string'."
-  (let ((mystr str))
+  (let ((myStr str))
     (mapc
-     (lambda (x) (setq mystr (replace-regexp-in-string (elt x 0) (elt x 1) mystr fixedcase)))
+     (lambda (x) (setq myStr (replace-regexp-in-string (elt x 0) (elt x 1) myStr fixedcase)))
      pairs)
-    mystr))
+    myStr))
 
-(defun replace-pairs-region (start end pairs)
+(defun replace-pairs-region-old (p1 p2 pairs)
   "Replace string find/replace PAIRS in region.
 The replacement is done only if there are changes
 made. (i.e. only if the find strings occurs in the region.)
 
 For detail, see `replace-pairs-in-string'."
   (let (inputStr newStr)
-    (setq inputStr (buffer-substring-no-properties start end))
+    (setq inputStr (buffer-substring-no-properties p1 p2))
     (setq newStr (replace-pairs-in-string inputStr pairs))
 
     (when (not (string-equal inputStr newStr))
-      (delete-region start end)
+      (delete-region p1 p2)
       (insert newStr)
       )
     ))
 
-(defun replace-regexp-pairs-region (start end pairs &optional fixedcase)
+(defun replace-pairs-region (p1 p2 pairs)
+  "Replace string find/replace PAIRS in region.
+
+Same as `replace-pairs-in-string' except does on a region."
+  (let (ξi (tempMapPoints '()))
+    ;; generate a random string list for intermediate replacement
+    (setq ξi 0)
+    (while (< ξi (length pairs))
+      (setq tempMapPoints (cons (format "⚎ด%x" ξi) tempMapPoints ))
+      (setq ξi (1+ ξi))
+      )
+    (save-excursion 
+      (narrow-to-region p1 p2)
+
+      ;; replace each find string by corresponding item in random string list
+      (setq ξi 0)
+      (while (< ξi (length pairs))
+        (goto-char (point-min))
+        (while (search-forward (elt (elt pairs ξi) 0) nil t)
+          (replace-match (elt tempMapPoints ξi) t t) )
+        (setq ξi (1+ ξi))
+        )
+
+      ;; replace each random string by corresponding replacement string
+      (setq ξi 0)
+      (while (< ξi (length pairs))
+        (goto-char (point-min))
+        (while (search-forward (elt tempMapPoints ξi) nil t)
+          (replace-match (elt (elt pairs ξi) 1) t t) )
+        (setq ξi (1+ ξi)) ) 
+      (widen)
+) ) )
+
+(defun replace-regexp-pairs-region (p1 p2 pairs &optional fixedcase)
   "Replace regex string find/replace PAIRS in region.
 
 For detail, see `replace-regexp-pairs-in-string'."
-  (let (mystr)
-    (setq mystr (buffer-substring-no-properties start end))
-    (delete-region start end)
-    (insert (replace-regexp-pairs-in-string mystr pairs fixedcase))))
+  (let (myStr)
+    (setq myStr (buffer-substring-no-properties p1 p2))
+    (delete-region p1 p2)
+    (insert (replace-regexp-pairs-in-string myStr pairs fixedcase))))
 
 (defun replace-pairs-in-string-recursive (str pairs)
   "Replace string STR recursively by find/replace pairs PAIRS sequence.
@@ -160,11 +192,11 @@ but replace-pairs-in-string-recursive would return
 “dbdd”.
 
 See `replace-pairs-in-string' for full doc."
-  (let (mystr)
-    (setq mystr str)
+  (let (myStr)
+    (setq myStr str)
     (mapc
-     (lambda (x) (setq mystr (replace-regexp-in-string (regexp-quote (elt x 0)) (elt x 1) mystr t t)))
+     (lambda (x) (setq myStr (replace-regexp-in-string (regexp-quote (elt x 0)) (elt x 1) myStr t t)))
      pairs)
-    mystr))
+    myStr))
 
 (provide 'xfrp_find_replace_pairs)
