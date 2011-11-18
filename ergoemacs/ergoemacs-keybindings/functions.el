72bdf2840e736883ad4f6de181aa815b69ba16f2
xahlee@gmail.com <xahlee@gmail.com@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2011-11-18 09:10:29 +0000

    ergoemacs-keybindings/functions.el minor improvemnt on â€œtoggle-letter-caseâ€. Now better guess at initial state.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@772 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/ergoemacs-keybindings/functions.el
+++ ergoemacs/ergoemacs-keybindings/functions.el
@@ -260,31 +260,33 @@ Calling this command 3 times will always result in no whitespaces around cursor.
 
 (defun toggle-letter-case ()
   "Toggle the letter case of current word or text selection.
-Toggles from 3 cases: UPPER CASE, lower case, Title Case,
-in that cyclic order."
-(interactive)
-(let (pos1 pos2 (deactivate-mark nil) (case-fold-search nil))
-  (if (region-active-p)
-      (setq pos1 (region-beginning) pos2 (region-end))
-    (let ((bds (bounds-of-thing-at-point 'word) ) )
-      (setq pos1 (car bds) pos2 (cdr bds)) ) )
+Toggles between: “all lower”, “Init Caps”, “ALL CAPS”."
+  (interactive)
+  (let (p1 p2 (deactivate-mark nil) (case-fold-search nil))
+    (if (region-active-p)
+        (setq p1 (region-beginning) p2 (region-end))
+      (let ((bds (bounds-of-thing-at-point 'word) ) )
+        (setq p1 (car bds) p2 (cdr bds)) ) )
 
-  (when (not (eq last-command this-command))
-    (save-excursion
-      (goto-char pos1)
-      (cond
-       ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
-       ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps") )
-       ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps") )
-       (t (put this-command 'state "all lower") ) ) ) )
+    (when (not (eq last-command this-command))
+      (save-excursion
+        (goto-char p1)
+        (cond
+         ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
+         ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps") )
+         ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps") )
+         ((looking-at "[[:lower:]]") (put this-command 'state "all lower"))
+         ((looking-at "[[:upper:]]") (put this-command 'state "all caps") )
+         (t (put this-command 'state "all lower") ) ) ) )
 
-  (cond
-   ((string= "all lower" (get this-command 'state))
-    (upcase-initials-region pos1 pos2) (put this-command 'state "init caps"))
-   ((string= "init caps" (get this-command 'state))
-    (upcase-region pos1 pos2) (put this-command 'state "all caps"))
-   ((string= "all caps" (get this-command 'state))
-    (downcase-region pos1 pos2) (put this-command 'state "all lower")) ) ) )
+    (cond
+     ((string= "all lower" (get this-command 'state))
+      (upcase-initials-region p1 p2) (put this-command 'state "init caps"))
+     ((string= "init caps" (get this-command 'state))
+      (upcase-region p1 p2) (put this-command 'state "all caps"))
+     ((string= "all caps" (get this-command 'state))
+      (downcase-region p1 p2) (put this-command 'state "all lower")) )
+    ) )
 
 ;;; FRAME
 
