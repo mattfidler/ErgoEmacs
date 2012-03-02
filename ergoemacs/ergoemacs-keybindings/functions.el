7a76e166765444633a42726ecd46b3e1ed60cb58
xahlee@gmail.com <xahlee@gmail.com@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2012-03-02 07:10:18 +0000

    ErgoEmacs keybinding functions.el. Improved code for â€œcompact-uncompact-blockâ€. No behavior change.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@786 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/ergoemacs-keybindings/functions.el
+++ ergoemacs/ergoemacs-keybindings/functions.el
@@ -138,64 +138,32 @@ If there's no text, delete the previous line ending."
   )
 
 (defun compact-uncompact-block ()
-  "Remove or add line endings on the current block of text.
-This is similar to a toggle for fill-paragraph and unfill-paragraph
-When there is a text selection, act on the region.
-
-When in text mode, a paragraph is considerd a block. When in programing
-language mode, the block is defined by between empty lines.
-
-Todo: The programing language behavior is currently not done.
-Right now, the code uses fill* functions, so does not work or work well
-in programing lang modes. A proper implementation to compact is replacing
-EOL chars by space when the EOL char is not inside string."
+  "Remove or add line ending chars on current paragraph.
+This command is similar to a toggle of `fill-paragraph'.
+When there is a text selection, act on the region."
   (interactive)
 
-  ;; This command symbol has a property “'stateIsCompact-p”, the
-  ;; possible values are t and nil. This property is used to easily
-  ;; determine whether to compact or uncompact, when this command is
-  ;; called again
-
-  (let (bds currentLineCharCount currentStateIsCompact
-            (bigFillColumnVal 4333999) (deactivate-mark nil))
+  ;; This command symbol has a property “'stateIsCompact-p”.
+  (let (currentStateIsCompact (bigFillColumnVal 4333999) (deactivate-mark nil))
 
     (save-excursion
-      ;; currentLineCharCount is used to determine whether current state
-      ;; is compact or not, when the command is run for the first time
-      (setq currentLineCharCount
-            (progn
-              (setq bds (bounds-of-thing-at-point 'line))
-              (length (buffer-substring-no-properties (car bds) (cdr bds)))    
-              ;; Note: 'line includes eol if it is not buffer's last line
-              )
-            )
-
-      ;; Determine whether the text is currently compact.  when the last
-      ;; command is this, then symbol property easily tells, but when
-      ;; this command is used fresh, right now we use num of chars of
-      ;; the cursor line as a way to define current compatness state
+      ;; Determine whether the text is currently compact.
       (setq currentStateIsCompact
             (if (eq last-command this-command)
                 (get this-command 'stateIsCompact-p)
-              (if (> currentLineCharCount fill-column) t nil)
-              )
-            )
+              (if (> (- (line-end-position) (line-beginning-position)) fill-column) t nil) ) )
 
-      (if (and transient-mark-mode mark-active)
+      (if (region-active-p)
           (if currentStateIsCompact
               (fill-region (region-beginning) (region-end))
             (let ((fill-column bigFillColumnVal))
-              (fill-region (region-beginning) (region-end)))
-            )
+              (fill-region (region-beginning) (region-end))) )
         (if currentStateIsCompact
             (fill-paragraph nil)
           (let ((fill-column bigFillColumnVal))
-            (fill-paragraph nil))
-          )
-        )
+            (fill-paragraph nil)) ) )
 
-      (put this-command 'stateIsCompact-p (if currentStateIsCompact
-                                              nil t)) ) ) )
+      (put this-command 'stateIsCompact-p (if currentStateIsCompact nil t)) ) ) )
 
 (defun shrink-whitespaces ()
   "Remove white spaces around cursor to just one or none.
