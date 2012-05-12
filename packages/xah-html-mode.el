24f5efaad7e4beb98f9452056fa9d12ce2224b4b
xahlee@gmail.com <xahlee@gmail.com@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2012-05-12 19:51:51 +0000

    xah-html-mode.el update.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@832 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- packages/xah-html-mode.el
+++ packages/xah-html-mode.el
@@ -2,13 +2,15 @@
 ;; xah-html-mode.el -- Major mode for editing pure html5.
 
 ;;; HISTORY
-;; version 0.1, 2011-07-29
+;; version 0.2, 2012-05-12 first version 
 
 (defvar xah-html-mode-hook nil "Standard hook for `xah-html-mode'")
 
 (defvar xhm-ele-names nil "HTML tag names list.")
 (setq xhm-ele-names
-'("a" "abbr" "acronym" "address" "applet" "area" "article" "aside" "audio" "b" "base" "basefont" "bdi" "bdo" "bgsound" "big" "blockquote" "body" "br" "button" "canvas" "caption" "center" "cite" "code" "col" "colgroup" "command" "datalist" "dd" "del" "details" "dfn" "dir" "div" "dl" "dt" "em" "embed" "fieldset" "figcaption" "figure" "font" "footer" "form" "frame" "frameset" "h1" "h2" "h3" "h4" "h5" "h6" "head" "header" "hgroup" "hr" "html" "i" "iframe" "img" "input" "ins" "kbd" "keygen" "label" "legend" "li" "link" "map" "mark" "menu" "meta" "meter" "nav" "noframes" "noscript" "object" "ol" "optgroup" "option" "output" "p" "param" "pre" "progress" "q" "rp" "rt" "ruby" "s" "samp" "script" "section" "select" "small" "source" "span" "strike" "strong" "style" "sub" "summary" "sup" "table" "tbody" "td" "textarea" "tfoot" "th" "thead" "time" "title" "tr" "tt" "u" "ul" "var" "video" "wbr" "xmp" ))
+'("a" "abbr" "acronym" "address" "applet" "area" "article" "aside" "audio" "b" "base" "basefont" "bdi" "bdo" "bgsound" "big" "blockquote" "body" "br" "button" "canvas" "caption" "center" "cite" "code" "col" "colgroup" "command" "datalist" "dd" "del" "details" "dfn" "dir" "div" "dl" "dt" "em" "embed" "fieldset" "figcaption" "figure" "font" "footer" "form" "frame" "frameset" "h1" "h2" "h3" "h4" "h5" "h6" "head" "header" "hgroup" "hr" "html" "i" "iframe" "img" "input" "ins" "kbd" "keygen" "label" "legend" "li" "link" "map" "mark" "menu" "meta" "meter" "nav" "noframes" "noscript" "object" "ol" "optgroup" "option" "output" "p" "param" "pre" "progress" "q" "rp" "rt" "ruby" "s" "samp" "script" "section" "select" "small" "source" "span" "strike" "strong" "style" "sub" "summary" "sup" "table" "tbody" "td" "textarea" "tfoot" "th" "thead" "time" "title" "tr" "tt" "u" "ul" "var" "video" "wbr" "xmp" 
+"doctype"
+))
 (defvar xhm-ele-names-regexp nil "regex for font locking")
 (setq xhm-ele-names-regexp (regexp-opt xhm-ele-names 'words))
 (setq xhm-ele-names nil)                 ; no longer needed
@@ -16,12 +18,19 @@
 (defvar xhm-attr-names nil "HTML attributes name list.")
 (setq xhm-attr-names
 '(
-"id"
-"class"
-"style"
-"title"
-"width"
-"height"
+"id="
+"class="
+"style="
+"title="
+
+"href="
+"type="
+"rel="
+"http-equiv="
+"content="
+"charset="
+"width="
+"height="
  ))
 (defvar xhm-attr-names-regexp nil "regex for font locking")
 (setq xhm-attr-names-regexp (regexp-opt xhm-attr-names 'words))
@@ -40,7 +49,7 @@
 ;; define the mode
 (define-derived-mode xah-html-mode fundamental-mode
   "xah-html"
-  "Major mode for editing LSL (Linden Scripting Language)…"
+  "Major mode for HTML."
 
   (setq font-lock-defaults '((xhm-font-lock-keywords)))
 
