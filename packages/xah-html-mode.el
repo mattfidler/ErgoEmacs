96c21030c0b3d26e0b52347d2c39334f0c9a04f4
xahlee@gmail.com <xahlee@gmail.com@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2012-05-13 12:32:03 +0000

    xah-html-mode.el added comment handling. improved syntax coloring. Added keymap and syntax table.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@835 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- packages/xah-html-mode.el
+++ packages/xah-html-mode.el
@@ -2,48 +2,57 @@
 ;; xah-html-mode.el -- Major mode for editing pure html5.
 
 ;;; HISTORY
-;; version 0.2, 2012-05-12 first version 
+;; version 0.3, 2012-05-13 added comment handling. improved syntax coloring. Added keymap and syntax table."
+;; version 0.2, 2012-05-12 first version
 
 (defvar xah-html-mode-hook nil "Standard hook for `xah-html-mode'")
 
-(defvar xhm-ele-names nil "HTML tag names list.")
-(setq xhm-ele-names
-'("a" "abbr" "acronym" "address" "applet" "area" "article" "aside" "audio" "b" "base" "basefont" "bdi" "bdo" "bgsound" "big" "blockquote" "body" "br" "button" "canvas" "caption" "center" "cite" "code" "col" "colgroup" "command" "datalist" "dd" "del" "details" "dfn" "dir" "div" "dl" "dt" "em" "embed" "fieldset" "figcaption" "figure" "font" "footer" "form" "frame" "frameset" "h1" "h2" "h3" "h4" "h5" "h6" "head" "header" "hgroup" "hr" "html" "i" "iframe" "img" "input" "ins" "kbd" "keygen" "label" "legend" "li" "link" "map" "mark" "menu" "meta" "meter" "nav" "noframes" "noscript" "object" "ol" "optgroup" "option" "output" "p" "param" "pre" "progress" "q" "rp" "rt" "ruby" "s" "samp" "script" "section" "select" "small" "source" "span" "strike" "strong" "style" "sub" "summary" "sup" "table" "tbody" "td" "textarea" "tfoot" "th" "thead" "time" "title" "tr" "tt" "u" "ul" "var" "video" "wbr" "xmp" 
-"doctype"
-))
-(defvar xhm-ele-names-regexp nil "regex for font locking")
-(setq xhm-ele-names-regexp (regexp-opt xhm-ele-names 'words))
-(setq xhm-ele-names nil)                 ; no longer needed
-
-(defvar xhm-attr-names nil "HTML attributes name list.")
-(setq xhm-attr-names
-'(
-"id"
-"class"
-"style"
-"title"
-"href"
-"type"
-"rel"
-"http-equiv"
-"content"
-"charset"
-"width"
-"height"
- ))
-(defvar xhm-attr-names-regexp nil "regex for font locking")
-(setq xhm-attr-names-regexp (regexp-opt xhm-attr-names 'words))
-(setq xhm-attr-names nil)                 ; no longer needed
+
+;; syntax coloring related
 
 (setq xhm-font-lock-keywords
-  `(
-    (,xhm-ele-names-regexp . (1 font-lock-function-name-face))
-    (,xhm-attr-names-regexp . (1 font-lock-keyword-face))
+(let (
+(htmlElementNamesRegex (regexp-opt '("a" "abbr" "acronym" "address" "applet" "area" "article" "aside" "audio" "b" "base" "basefont" "bdi" "bdo" "bgsound" "big" "blockquote" "body" "br" "button" "canvas" "caption" "center" "cite" "code" "col" "colgroup" "command" "datalist" "dd" "del" "details" "dfn" "dir" "div" "dl" "dt" "em" "embed" "fieldset" "figcaption" "figure" "font" "footer" "form" "frame" "frameset" "h1" "h2" "h3" "h4" "h5" "h6" "head" "header" "hgroup" "hr" "html" "i" "iframe" "img" "input" "ins" "kbd" "keygen" "label" "legend" "li" "link" "map" "mark" "menu" "meta" "meter" "nav" "noframes" "noscript" "object" "ol" "optgroup" "option" "output" "p" "param" "pre" "progress" "q" "rp" "rt" "ruby" "s" "samp" "script" "section" "select" "small" "source" "span" "strike" "strong" "style" "sub" "summary" "sup" "table" "tbody" "td" "textarea" "tfoot" "th" "thead" "time" "title" "tr" "tt" "u" "ul" "var" "video" "wbr" "xmp" "doctype") 'words))
+(AttributeNamesRegexp (regexp-opt '( "id" "class" "style" "title" "href" "type" "rel" "http-equiv" "content" "charset" "alt" "src" "width" "height" ) 'words))
+ )
+`(
+(,htmlElementNamesRegex . font-lock-function-name-face)
+(,AttributeNamesRegexp . font-lock-keyword-face)
+("“\\([^ ]+?\\)”" . (1 font-lock-string-face))
+("「\\([^」]+\\)」" . (1 font-lock-string-face))
+("<!--\\|-->" . font-lock-comment-delimiter-face)
+("<!--\\([^-]+?\\)-->" . (1 font-lock-comment-face))
+) ) )
 
-    ("“\\([^ ]+?\\)”" . (1 font-lock-string-face))
-    ("「\\([^」]+\\)」" . (1 font-lock-string-face))
+
+;; keybinding
 
-))
+(defvar xhm-keymap nil "Keybinding for `xah-html-mode'")
+(progn
+  (setq xhm-keymap (make-sparse-keymap))
+  (define-key xhm-keymap [remap comment-dwim] 'xhm-comment-dwim)
+  )
+
+
+;; syntax table
+(defvar xhm-syntax-table nil "Syntax table for `xah-html-mode'.")
+(setq xhm-syntax-table
+      (let ((synTable (make-syntax-table)))
+        (modify-syntax-entry ?< "." synTable)
+        (modify-syntax-entry ?> "." synTable)
+        synTable))
+
+
+
+(defun xhm-comment-dwim (arg)
+"Comment or uncomment current line or region in a smart way.
+For detail, see `comment-dwim'."
+   (interactive "*P")
+   (require 'newcomment)
+   (let ((deactivate-mark nil) (comment-start "<!--") (comment-end "-->"))
+     (comment-dwim arg)))
+
+
 
 ;; define the mode
 (define-derived-mode xah-html-mode fundamental-mode
@@ -52,12 +61,10 @@
 
   (setq font-lock-defaults '((xhm-font-lock-keywords)))
 
-  (setq xhm-ele-names-regexp nil)       ; no longer needed
-  (setq xhm-attr-names-regexp nil)       ; no longer needed
+  (set-syntax-table xhm-syntax-table)
+  (use-local-map xhm-keymap)
 
-  (require 'sgml-mode )
-
-  (setq mode-name "xah-html")
+;;  (setq mode-name "xah-html")
   (run-mode-hooks 'xah-html-mode-hook)
 )
 
