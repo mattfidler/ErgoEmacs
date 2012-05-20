364363a2b35c457aba29a7d5ed0b1b4d46e14325
xahlee@gmail.com <xahlee@gmail.com@f1309a34-a24b-11de-ba4d-5dbaeb624ccf>
2012-05-20 10:30:32 +0000

    ErgoEmacs keybinding. ** added ã€alt+Delã€‘ to delete word to the right.
    git-svn-id: https://ergoemacs.googlecode.com/svn/trunk@856 f1309a34-a24b-11de-ba4d-5dbaeb624ccf

--- ergoemacs/ergoemacs-keybindings/ergoemacs-mode.el
+++ ergoemacs/ergoemacs-keybindings/ergoemacs-mode.el
@@ -5,7 +5,7 @@
 ;; Copyright © 2009, 2010 by David Capello
 
 ;; Author: Xah Lee ( http://xahlee.org/ ), David Capello ( http://www.davidcapello.com.ar/ )
-;; Version: 5.4.7
+;; Version: See the variable ergoemacs-mode-version.
 ;; Keywords: qwerty, dvorak, keybinding, ergonomic, colemak
 
 ;; You can redistribute this program and/or modify it under the terms
@@ -55,7 +55,7 @@
 (add-to-list 'load-path (file-name-directory (or load-file-name buffer-file-name)))
 
 ;; Ergoemacs-keybindings version
-(defconst ergoemacs-mode-version "5.4.7"
+(defconst ergoemacs-mode-version "5.4.8"
   "Ergoemacs-keybindings minor mode version number.")
 
 ;; Include extra files
@@ -295,6 +295,8 @@ Valid values are:
   t )
  ((string-equal system-type "gnu/linux")
   t ) )
+
+(define-key ergoemacs-keymap (kbd "<M-delete>") 'kill-word)
 
 ;; arrow keys to traverse brackets
 (define-key ergoemacs-keymap (kbd "<M-left>") 'backward-open-bracket) ; Alt+←
