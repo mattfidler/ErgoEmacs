fe7158fb3dce0827cc88acc7ca706b7c65b14d74
Xah Lee <xah@xahlee.org>
2012-09-12 09:25:34 -0700

    init_settings.el removed custome color for i-search word

--- ergoemacs/init_settings.el
+++ ergoemacs/init_settings.el
@@ -256,5 +256,5 @@
  ;; '(diredp-ignored-file-name ((t (:foreground "#bebebe"))))
  '(diredp-compressed-file-suffix ((t (:foreground "#7b68ee"))))
  '(diredp-ignored-file-name ((t (:foreground "#aaaaaa"))))
- '(isearch ((((class color) (min-colors 88) (background light)) (:background "black" :foreground "white"))))
+ ;; '(isearch ((((class color) (min-colors 88) (background light)) (:background "black" :foreground "white"))))
  '(show-paren-match ((((class color) (background light)) (:background "azure2")))))
