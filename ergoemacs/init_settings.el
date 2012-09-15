cf20ce812ef985f7bfa1c94ccb7ab81bad50d872
Xah Lee <xah@xahlee.org>
2012-09-15 04:22:06 -0700

    init_settings.el improved config for whitespace-mode. Removed bad config char code in default value of whitespace-display-mappings

--- ergoemacs/init_settings.el
+++ ergoemacs/init_settings.el
@@ -202,19 +202,16 @@
 ;; § ----------------------------------------
 
 (progn
-  ;; Make whitespace-mode with very basic background coloring for whitespaces
+  ;; Make whitespace-mode with very basic background coloring for whitespaces.
+  ;; http://ergoemacs.org/emacs/whitespace-mode.html
   (setq whitespace-style (quote ( spaces tabs newline space-mark tab-mark newline-mark )))
 
-  ;; Make whitespace-mode and whitespace-newline-mode use “¶” for end of line char and ▷ for tab.
+  ;; Make whitespace-mode and whitespace-newline-mode use “¶” for end of line char and “▷” for tab.
   (setq whitespace-display-mappings
+        ;; all numbers are unicode codepoint in decimal. e.g. (insert-char 182 1)
         '(
-          (space-mark 32 [183] [46]) ; normal space, MIDDLE DOT, FULL STOP.
-          (space-mark 160 [164] [95])
-          (space-mark 2208 [2212] [95])
-          (space-mark 2336 [2340] [95])
-          (space-mark 3616 [3620] [95])
-          (space-mark 3872 [3876] [95])
-          (newline-mark 10 [182 10]) ; newlne
+          (space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+          (newline-mark 10 [182 10]) ; LINE FEED, 
           (tab-mark 9 [9655 9] [92 9]) ; tab
           )) )
 
