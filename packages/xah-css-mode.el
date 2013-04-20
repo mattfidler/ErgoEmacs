1cb318b273037f6b63af26b24e3d6c8343a23c12
Xah Lee <xah@xahlee.org>
2013-04-19 18:45:07 -0700

    xah-css-mode.el added xcm-insert-random-color-hsl

--- packages/xah-css-mode.el
+++ packages/xah-css-mode.el
@@ -17,6 +17,15 @@
 
 (defvar xah-css-mode-hook nil "Standard hook for `xah-css-mode'")
 
+
+
+(defun xcm-insert-random-color-hsl ()
+  "Insert a random color string of CSS HSL format.
+Example output: hsl(100,24%,82%)"
+  (interactive)
+  (insert (format "hsl(%d,%d%%,%d%%)" (random 360) (random 100) (random 100))) )
+
+
 (defvar xcm-html-tag-names nil "a list of HTML5 tag names.")
 (setq xcm-html-tag-names
 '("a" "abbr" "acronym" "address" "applet" "area" "article" "aside" "audio" "b" "base" "basefont" "bdi" "bdo" "bgsound" "big" "blockquote" "body" "br" "button" "canvas" "caption" "center" "cite" "code" "col" "colgroup" "command" "datalist" "dd" "del" "details" "dfn" "dir" "div" "dl" "dt" "em" "embed" "fieldset" "figcaption" "figure" "font" "footer" "form" "frame" "frameset" "h1" "h2" "h3" "h4" "h5" "h6" "head" "header" "hgroup" "hr" "html" "i" "iframe" "img" "input" "ins" "kbd" "keygen" "label" "legend" "li" "link" "map" "mark" "menu" "meta" "meter" "nav" "noframes" "noscript" "object" "ol" "optgroup" "option" "output" "p" "param" "pre" "progress" "q" "rp" "rt" "ruby" "s" "samp" "script" "section" "select" "small" "source" "span" "strike" "strong" "style" "sub" "summary" "sup" "table" "tbody" "td" "textarea" "tfoot" "th" "thead" "time" "title" "tr" "tt" "u" "ul" "var" "video" "wbr" "xmp" "doctype")
