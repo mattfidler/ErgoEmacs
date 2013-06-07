be8df2fe75f12c0a13a3bd9582f2694b359f6a1f
Xah Lee <xah@xahlee.org>
2013-06-06 19:50:37 -0700

    ErgoEmacs init_settings.el prevent cursor going into promp.

--- ergoemacs/init_settings.el
+++ ergoemacs/init_settings.el
@@ -24,6 +24,9 @@
 ;; Save minibuffer history
 (savehist-mode 1)
 
+;; don't let the cursor go into minibuffer prompt
+(setq minibuffer-prompt-properties (quote (read-only t point-entered minibuffer-avoid-prompt face minibuffer-prompt)))
+
 ;; turn on save place so that when opening a file, the cursor will be at the last position.
 (require 'saveplace)
 (setq save-place-file (concat user-emacs-directory "saveplace.el") ) ; use standard emacs dir
